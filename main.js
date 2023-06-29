f28f08dd4d78f9f72969f487b47e109448a9abd6
skdhagskdjhgaskdjhgasdsad <102916554+skdhagskdjhgaskdjhgasdsad@users.noreply.github.com>
2023-06-29 09:26:17 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+
+clown_nose = "" ;
+
+function preload() {
+  clown_nose = loadImage('funny.png');
+}
+
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+
+  video = createCapture(VIDEO);
+  video.size(300, 300);
+  video.hide();
+
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+  console.log('PoseNet Is Initialized');
+}
+
+noseX=0;
+noseY=0;
+
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+    console.log(results);
+    noseX = results[0].pose.nose.x;
+    noseY = results[0].pose.nose.y;
+    console.log("nose x= " + noseX +"nose Y= " + noseY);
+  }
+}
+
+function draw() {
+  image(video, 0, 0, 300, 300);
+  image(clown_nose, noseX-15, noseY-0, 30, 30);
+  
+}
+
+function take_snapshot(){    
+  save('myFilterImage.png');
+}
